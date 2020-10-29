8f5dd256980bbbafe2599c15789f9543986b63c3
Robert Seidel <robertseidel@mail.de>
2020-10-29 14:36:04 +0100

    Added function for fetching individual posts

--- pages/post.php
+++ pages/post.php
@@ -3,6 +3,18 @@
 
     <h1>Post.php</h1>
 
-    <pre><?php var_dump($_GET); ?></pre>
+    <?php
+        $title = $_GET['title'];
+        $post = fetch_post($title);
+    ?>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title"><?php echo $post["title"]; ?></h3>
+        </div>
+        <div class="panel-body">
+            <?php echo $post["content"]; ?>
+        </div>
+    </div>
 
 <?php include("elements/footer.php"); ?>
